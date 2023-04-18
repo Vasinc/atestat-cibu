4ef142e397ee095137283521d64e4a32cd4493ff
Vasinc <lein0.jade@gmail.com>
2023-04-19 01:14:18 +0300

    3 ( sections added )

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,11 +7,19 @@ const hackEffectTexts = document.querySelectorAll('.hack-effect__text');
 
 const letters = "abcdefghijklmnopqrstuvwxyz"
 
+let previousPage = 'Pagina-principală';
+
 // functions
 function toggleHeaderUl () {
     burgerMenu.classList.toggle('burger-active')
     backdrop.classList.toggle('display-block')
     headerUl.classList.toggle('display-flex')
+    if(headerUl.classList.contains('display-flex')) {
+        backdrop.scrollIntoView();
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = 'visible';
+    }
     setTimeout(() => {
         headerUl.classList.toggle('width-70')
     }, 1);
@@ -32,6 +40,53 @@ headerUl.addEventListener('click', event => {
     if (event.target.tagName != 'LI') return;
 
     toggleHeaderUl();
+
+
+    // changes sections and changes previousPage
+    const liValue = event.target.dataset.value.split(' ').join('-');
+    const previousSection = document.getElementById(`${previousPage}`);
+    previousSection.classList.remove('display-block');
+    const currentSection = document.getElementById(`${liValue}`);
+    currentSection.classList.add('display-block');
+    previousPage = liValue;
+
+    // changes h1 html
+    const sectionH1 = currentSection.querySelector('h1');
+    const sectionH1Letters = sectionH1.innerText.split('');
+
+    sectionH1.innerHTML = '';
+
+    sectionH1Letters.forEach(letter => {
+        sectionH1.innerHTML += `<span class="h1-letter">${letter}</span>`
+    })
+        
+    const H1Letters = sectionH1.querySelectorAll('.h1-letter');
+    
+    console.log(H1Letters)
+
+    let iterations = 0;
+
+    const interval = setInterval(() => {
+        
+        H1Letters[iterations].classList.add('opacity-1')
+
+        if(iterations >= sectionH1Letters.length - 1) {
+            clearInterval(interval);
+        }
+
+        iterations += 1;
+
+    }, 100);
+
+
+    // updates Header UL visually
+    headerLi.forEach(li => {
+        if ( li  == event.target ) {
+            li.classList.add('active-li');
+        } else {
+            li.classList.remove('active-li');
+        }
+    })
 })
 
 hackEffectTexts.forEach(hackEffectText => {
